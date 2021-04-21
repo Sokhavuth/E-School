d943cf5f6748849415b876f8242510ccaa80f12f
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-21 18:24:05 +0700

    Make it better

--- eschool/models.py
+++ eschool/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import uuid
+from djongo import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Lesson(models.Model):
+  CHOICES = (
+    ('YT', 'YouTube'),
+    ('FB', 'Facebook'),
+    ('OK', 'Одноклассники'),
+    ('DM', 'Dailymotion'),
+    ('VM', 'Vimeo'),
+  )
+
+  lesson_id = models.UUIDField(editable=False, default=uuid.uuid4, unique=True)
+  title = models.CharField(max_length=255)
+  published_date = models.DateTimeField()
+  categories = models.TextField()
+  content = models.TextField()
+  video_type = models.CharField(max_length=255, choices = CHOICES, default=1)
+  video_id = models.CharField(max_length=255, blank=True)
+  published_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+  
--- eschool/views.py
+++ eschool/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lesson
 
 context = {'site_title': 'E-School'}
 
 def index(request):
+    lessons = Lesson.objects.all()[:4]
+    video_ids = [lesson.video_id for lesson in lessons]
+    context['video_ids'] = video_ids
+
     return render(request, 'index.html', context=context)
